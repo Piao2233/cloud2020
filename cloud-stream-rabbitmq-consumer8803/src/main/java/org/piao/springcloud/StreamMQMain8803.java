de3582c6e98832508c869c11b9676654d4c1617b
ZhaoYing Cheng <2119828525@qq.com>
2023-01-04 20:12:16 +0800

    spring cloud stream之解决消息重复消费

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/org/piao/springcloud/StreamMQMain8803.java
@@ -0,0 +1,16 @@
+package org.piao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author：piao
+ * @Date：2023/1/2 10:21 PM
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
